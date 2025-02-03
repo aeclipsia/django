d4fa100b8985af3301cc6ffae2abeddf51cdc65c
administrador <administrador@Xubuntu22.salesianas.org>
2025-02-03 13:47:33 +0100

    03/02/2025

--- Practica10/Practica10/views.py
+++ Practica10/Practica10/views.py
@@ -8,7 +8,22 @@ def index(request):
         form=Formulario(request.POST)
         if form.is_valid():
             collection=form.cleaned_data
-            return render(request,'details.html',collection)
+            
+            countAficiones=len(collection["aficiones"])
+            
+            if countAficiones==0:
+                mensaje="Eres un soso"
+            elif countAficiones==1:
+                print(["aficiones"][0])
+                mensaje=collection["nombre"]+" deberías buscar más aficiones que "+collection["aficiones"][0]
+            elif countAficiones==5:
+                mensaje=collection["nombre"]+" creo que tienes demasiadas aficiones"
+            else:
+                mensaje=""
+                
+            context={"collection":collection,"mensaje":mensaje}
+            
+            return render(request,'details.html',context)
     else:
         form=Formulario()
     return render(request,"index.html",{'form':form})
--- Practica4/Practica4/urls.py
+++ Practica4/Practica4/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for Practica10 project.
+URL configuration for Practica4 project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
@@ -16,9 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from Practica10.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',index,name="index")
+    path('')
 ]
